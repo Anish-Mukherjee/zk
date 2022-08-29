23e4f2c40643181b8b3cbee07cdb64cdf43418ca
Anish <cybertronian217@gmail.com>
2022-08-29 13:33:30 +0530

    Init ✅

---
+++ components/Announcement.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Announcement = () => {
+  return (
+    <div className="px-4 py-3 text-white bg-indigo-600">
+      <p className="text-sm font-medium text-center">
+        Want to create the next generation privacy focused DAO? &nbsp;
+       <a className="underline" href="https://form.waitlistpanda.com/go/9NTj0XciVQFylqXrTdki" target="_blank"> Sign up here!</a>
+      </p>
+    </div>
+  );
+};
+
+export default Announcement;
---
+++ components/CTA.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CTA = () => {
+  return (
+    <div className="text-center bg-black h-screen">
+      <div className="text-9xl font-extrabold py-32">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+          Zero Knowledge
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CTA;
