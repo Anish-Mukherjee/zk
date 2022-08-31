d87faff4adf0c2f5138239c1f1efe84b2bbbf798
Anish <cybertronian217@gmail.com>
2022-08-31 14:33:12 +0530

    CTA changes

--- components/CTA.js
+++ components/CTA.js
@@ -2,11 +2,14 @@ import React from "react";
 
 const CTA = () => {
   return (
-    <div className="text-center bg-black h-screen">
-      <div className="text-9xl font-extrabold py-32">
+    <div className="text-center bg-gradient-to-r from-gray-700 via-gray-900 to-black min-h-screen">
+      <div className="text-6xl font-extrabold pt-32 pb-10">
         <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-          Zero Knowledge
+          Verifiable Anonymous On-Chain Voting
         </span>
+      </div>
+      <div className="text-5xl font-bold text-white">
+        Powered by zk-proofs
       </div>
     </div>
   );
